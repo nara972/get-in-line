4771b62163128581816f006670ce7dfb67810cae
nara972 <68370958+nara972@users.noreply.github.com>
2022-01-05 21:42:32 +0900

    domain objects and enums

---
+++ src/main/java/com/nara/getinline/constant/EventStatus.java
@@ -0,0 +1,5 @@
+package com.nara.getinline.constant;
+
+public enum EventStatus {
+    PENDING, OPENED, CLOSED, CANCELLED, ABORTED
+}
---
+++ src/main/java/com/nara/getinline/constant/PlaceType.java
@@ -0,0 +1,5 @@
+package com.nara.getinline.constant;
+
+public enum PlaceType {
+    COMMON, SPORTS, RESTAURANT, PARTY
+}
